5e152c4653c0cab21644b4ee021e3ac6a338d962
Abdallah12332 <moragame660@gmail.com>
2024-12-30 02:06:21 +0200

    Updates

--- assets/the_second_model/Scene.jsx
+++ assets/the_second_model/Scene.jsx
@@ -9,9 +9,8 @@ Title: Robot Shark
 
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
-
 export default function Model(props) {
-  const { nodes, materials } = useGLTF('./assets/the_second_model/scene.gltf')
+  const { nodes, materials } = useGLTF('./public/assets/the_second_model/scene.gltf')
   return (
     <group {...props} dispose={null}>
       <group rotation={[-0.053, 0, 0]}>
@@ -63,4 +62,4 @@ export default function Model(props) {
   )
 }
 
-useGLTF.preload('./assets/the_first_model/scene.gltf')
+useGLTF.preload('./public/assets/the_second_model/scene.gltf')
--- assets/the_third_model/Scene.jsx
+++ assets/the_third_model/Scene.jsx
@@ -10,9 +10,10 @@ Title: Paper pen one
 import React from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
 import { useEffect } from 'react'
+
 export default function Model(props) {
   const group = React.useRef()
-  const { nodes, materials, animations } = useGLTF('./assets/the_third_model/scene.gltf')
+  const { nodes, materials, animations } = useGLTF('./public/assets/the_third_model/scene.gltf')
   const { actions,names } = useAnimations(animations, group)
   useEffect(()=>{
     actions[names[0]].reset().fadeIn(0.5).play();
@@ -41,4 +42,4 @@ export default function Model(props) {
   )
 }
 
-useGLTF.preload('./assets/the_third_model/scene.gltf')
+useGLTF.preload('./public/assets/the_third_model/scene.gltf')
